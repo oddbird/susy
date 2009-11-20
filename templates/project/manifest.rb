9832b626350b18aa2dc52d71c3a7df3cbeb8aaa6
Eric Meyer <eric@oddbird.net>
2009-11-20 14:04:24 -0700

    CSS syntax, cleanup, doc/site/tutorial update

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -2,3 +2,4 @@ stylesheet 'screen.sass', :media => "screen, projection"
 stylesheet 'print.sass', :media => "print"
 stylesheet 'ie.sass', :media => "screen, projection"
 stylesheet '_base.sass'
+stylesheet '_defaults.sass'

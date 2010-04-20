b7b67178d8f48ad7ab216652d344d7bf76df5c71
Eric Meyer <eric@oddbird.net>
2010-04-20 02:14:11 -0600

    metadata to incorporate with compass command line

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -3,3 +3,20 @@ stylesheet 'print.scss', :media => "print"
 stylesheet 'ie.scss', :media => "screen, projection"
 stylesheet '_base.scss'
 stylesheet '_defaults.scss'
+
+description "Susy: a flexible static/fluid/elastic grid system native to compass."
+
+help %Q{
+Please see the Susy website for all documentation and tutorials:
+
+    http://www.oddbird.net/susy
+}
+
+welcome_message %Q{
+Please see the Susy website for all documentation and tutorials:
+
+    http://www.oddbird.net/susy
+
+To get started, set up your grid in the grid partial by following the inline instructions.
+}
+

6e26f53003490c3be93c57c3512484b1bc773fd3
Eric Meyer <eric@oddbird.net>
2011-04-25 14:19:30 -0600

    simplified lib structure

--- lib/susy.rb
+++ lib/susy.rb
@@ -1,1 +1,4 @@
-require File.join(File.dirname(__FILE__), 'susy', 'compass_plugin')
+require 'compass'
+Compass::Frameworks.register('susy',
+                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
+                             :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))

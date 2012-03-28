63be3f0b9d7fe86d264ce00814a0f26a4d52bfbc
Daniel Palmer <danny@dannyprose.com>
2012-03-27 17:51:27 -0700

    setup minification and cache busting on build

--- docs/config.rb
+++ docs/config.rb
@@ -71,13 +71,13 @@
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
   
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
   
   # Use relative URLs
   # activate :relative_assets

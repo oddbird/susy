a1e54ea618c2b2eb85e628d0f08adb3b3154c605
Eric Meyer <eric@oddbird.net>
2012-05-11 19:55:54 -0600

    simplify title, description

--- docs/lib/guide_helpers.rb
+++ docs/lib/guide_helpers.rb
@@ -4,7 +4,7 @@ module GuideHelpers
     if data.page.title
       title << data.page.title
     else
-      title << "The Un-obstruive, Responsive Grid for Designers"
+      title << "Manage the chaos."
     end
     title
   end

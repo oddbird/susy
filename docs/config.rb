5fc0fb53fb42e668bdfadccaa8096fc7c0aa2278
Eric Meyer <eric@oddbird.net>
2012-05-16 02:22:08 -0600

    ready to build

--- docs/config.rb
+++ docs/config.rb
@@ -104,7 +104,7 @@ configure :build do
   activate :cache_buster
   
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
   
   # Compress PNGs after build
   # First: gem install middleman-smusher

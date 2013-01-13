78f8555f57491bdef4d8d8fb79f924fda2aa6464
Sam Richard <snugug@gmail.com>
2013-01-13 15:43:21 -0500

    Added Split String Ruby function

--- lib/susy.rb
+++ lib/susy.rb
@@ -2,3 +2,19 @@ require 'compass'
 Compass::Frameworks.register('susy',
                              :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
                              :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
+
+module Sass::Script::Functions
+  def str_replace(needle, replace, haystack)
+    result = haystack.value.gsub(needle.value, replace.value)
+    Sass::Script::String.new(result)
+  end
+
+  def split_string(string, key)
+    items = string.value.split(" " + key.value + " ")
+    if items.count == 1
+      Sass::Script::Bool.new(false)
+    else
+      Sass::Script::List.new(items.map{|i| Sass::Script::String.new(i)}, :comma)
+    end
+  end
+end
--- test/config.rb
+++ test/config.rb
@@ -10,3 +10,19 @@ line_comments = false
 preferred_syntax = :scss
 output_style = :expanded
 relative_assets = true
+
+module Sass::Script::Functions
+  def str_replace(needle, replace, haystack)
+    result = haystack.value.gsub(needle.value, replace.value)
+    Sass::Script::String.new(result)
+  end
+
+  def split_string(string, key)
+    items = string.value.split(" " + key.value + " ")
+    if items.count == 1
+      Sass::Script::Bool.new(false)
+    else
+      Sass::Script::List.new(items.map{|i| Sass::Script::String.new(i)}, :comma)
+    end
+  end
+end

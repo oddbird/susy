4617333b0daad2b4025c9c3b817246574488aca5
Sam Richard <snugug@gmail.com>
2013-01-21 17:25:40 -0500

    Updated Required Gems, Added Ruby Compare function

--- lib/susy.rb
+++ lib/susy.rb
@@ -1,20 +1,47 @@
 require 'compass'
+require 'sassy-strings'
+require 'sassy-math'
+require 'breakpoint'
 Compass::Frameworks.register('susy',
                              :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
                              :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
 
 module Sass::Script::Functions
-  def str_replace(needle, replace, haystack)
-    result = haystack.value.gsub(needle.value, replace.value)
-    Sass::Script::String.new(result)
-  end
+  # Compare Media Queries
+  def mq_compare(mq1, mq2, operator)
+    mq1 = mq1.value.to_i
+    mq2 = mq2.value.to_i
 
-  def split_string(string, key)
-    items = string.value.split(" " + key.value + " ")
-    if items.count == 1
-      Sass::Script::Bool.new(false)
+    if operator.value == 'lte'
+      if mq1 <= mq2
+        Sass::Script::Bool.new(true)
+      else
+        Sass::Script::Bool.new(false)
+      end
+    elsif operator.value == 'gte'
+      if mq1 >= mq2
+        Sass::Script::Bool.new(true)
+      else
+        Sass::Script::Bool.new(false)
+      end
+    elsif operator.value == 'lt'
+      if mq1 < mq2
+        Sass::Script::Bool.new(true)
+      else
+        Sass::Script::Bool.new(false)
+      end
+    elsif operator.value == 'gt'
+      if mq1 > mq2
+        Sass::Script::Bool.new(true)
+      else
+        Sass::Script::Bool.new(false)
+      end
     else
-      Sass::Script::List.new(items.map{|i| Sass::Script::String.new(i)}, :comma)
+      if mq1 == mq2
+        Sass::Script::Bool.new(true)
+      else
+        Sass::Script::Bool.new(false)
+      end
     end
   end
 end

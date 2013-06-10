3fcffb7814643ef2953189362badcc92378c9d18
Eric Meyer <eric@oddbird.net>
2013-06-09 19:14:27 -0600

    Write columns unit tests using True

--- test/config.rb
+++ test/config.rb
@@ -1,4 +1,5 @@
 # Compass CSS framework config file
+require 'true'
 add_import_path '../sass'
 
 project_type = :stand_alone
@@ -9,13 +10,3 @@ line_comments = false
 preferred_syntax = :scss
 output_style = :expanded
 relative_assets = true
-
-module Sass::Script::Functions
-
-  # Convert a grid piece from strings to numbers
-  def grid_to_numbers(columns)
-    items = columns.to_s.split(' ')
-    Sass::Script::List.new(items.map{|i| Sass::Script::Number.new(i.to_f)}, :comma)
-  end
-
-end

f6b0fd1054c00ca348f4acfa4d82affd94122af4
Eric Meyer <eric@oddbird.net>
2013-06-09 19:13:30 -0600

    Remove unused grid_to_numbers ruby function

--- lib/susy.rb
+++ lib/susy.rb
@@ -2,13 +2,3 @@ require 'compass'
 Compass::Frameworks.register('susy',
                              :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
                              :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
-
-module Sass::Script::Functions
-
-  # Convert a grid piece from strings to numbers
-  def grid_to_numbers(piece)
-    items = piece.to_s.split(' ')
-    Sass::Script::List.new(items.map{|i| Sass::Script::Number.new(i.to_f)}, :comma)
-  end
-
-end

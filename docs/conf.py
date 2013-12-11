4449a400617dba67dc96cdb9bc2e0cf503396b58
Eric Meyer <eric@oddbird.net>
2013-12-11 15:45:12 -0700

    Add RTD_NEW_THEME setting

--- docs/conf.py
+++ docs/conf.py
@@ -107,6 +107,8 @@ if not on_rtd:
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+RTD_NEW_THEME = True
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.

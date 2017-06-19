d18e0a0cc36ceaa86b0ec68893e60e5cfe4e1743
Miriam Suzanne <miriam@oddbird.net>
2017-06-19 10:25:54 -0600

    Update documentation

---
+++ assets/js/init.js
@@ -0,0 +1,1 @@
+window.Herman=function(i,n){"use strict";return n(function(){window.hljs.initHighlightingOnLoad(),i.initializeToggles(),i.initializeTabs(),i.initializeIframes()}),i}(window.Herman||{},window.jQuery);
